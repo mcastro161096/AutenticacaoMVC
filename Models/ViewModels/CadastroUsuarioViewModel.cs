6a3bb15e3b42cd8ef6692ff9a1509b1800f4eec9
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-06-13 22:26:50 -0300

    Criada ViewModel para cadastro de usuários.

---
+++ Models/ViewModels/CadastroUsuarioViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutenticacaoMVC.Models.ViewModels
+{
+    public class CadastroUsuarioViewModel
+    {
+        [Required(ErrorMessage ="Informe seu nome")]
+        [MaxLength(100, ErrorMessage ="O nome deve ter até {MaxLength} caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Informe seu login")]
+        [MaxLength(100, ErrorMessage = "O login deve ter até {MaxLength} caracteres")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Informe sua senha")]
+        [MinLength(6, ErrorMessage = "A senha deve ter no mínimo {MinLength} caracteres")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "Confirme sua senha")]
+        [MinLength(6, ErrorMessage = "A senha deve ter no mínimo {MinLength} caracteres")]
+        [DataType(DataType.Password)]
+        [Display(Name ="Confirme a senha")]
+        [Compare(nameof(Senha), ErrorMessage ="A senha e a confirmação não estão iguais")]
+        public string ConfirmacaoSenha { get; set; }
+    }
+}
