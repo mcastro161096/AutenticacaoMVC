64434df99300da65555a41c39609769a63d51d53
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-06-14 01:03:52 -0300

    Adicionado bootstrap e adicionado link para cadastrar na página de home.

--- Models/ViewModels/CadastroUsuarioViewModel.cs
+++ Models/ViewModels/CadastroUsuarioViewModel.cs
@@ -5,20 +5,20 @@ namespace AutenticacaoMVC.Models.ViewModels
     public class CadastroUsuarioViewModel
     {
         [Required(ErrorMessage ="Informe seu nome")]
-        [MaxLength(100, ErrorMessage ="O nome deve ter até {MaxLength} caracteres")]
+        [MaxLength(100, ErrorMessage ="O nome deve ter até 100 caracteres")]
         public string Nome { get; set; }
 
         [Required(ErrorMessage = "Informe seu login")]
-        [MaxLength(100, ErrorMessage = "O login deve ter até {MaxLength} caracteres")]
+        [MaxLength(100, ErrorMessage = "O login deve ter até 100 caracteres")]
         public string Login { get; set; }
 
         [Required(ErrorMessage = "Informe sua senha")]
-        [MinLength(6, ErrorMessage = "A senha deve ter no mínimo {MinLength} caracteres")]
+        [MinLength(6, ErrorMessage = "A senha deve ter no mínimo 6 caracteres")]
         [DataType(DataType.Password)]
         public string Senha { get; set; }
 
         [Required(ErrorMessage = "Confirme sua senha")]
-        [MinLength(6, ErrorMessage = "A senha deve ter no mínimo {MinLength} caracteres")]
+        [MinLength(6, ErrorMessage = "A senha deve ter no mínimo 6 caracteres")]
         [DataType(DataType.Password)]
         [Display(Name ="Confirme a senha")]
         [Compare(nameof(Senha), ErrorMessage ="A senha e a confirmação não estão iguais")]
