21a78b5c575d7979151ebfe9bbd5ed0233eac37e
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-06-13 21:58:27 -0300

    Configurando Statup do Owin e criando entidade central Usuarios.

---
+++ Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security.Cookies;
+
+[assembly: OwinStartup(typeof(AutenticacaoMVC.Startup))]
+
+namespace AutenticacaoMVC
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            { 
+                 AuthenticationType = "AplicationCookie",
+                 LoginPath = new PathString("/Autenticacao/Login")
+            });
+        }
+    }
+}
