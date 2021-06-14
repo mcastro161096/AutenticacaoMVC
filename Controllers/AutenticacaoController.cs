18bf11de1b784075fbbdf23e58b63aef45058926
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-06-13 22:48:11 -0300

    Criados Controller de autenticação e view para cadastro de usuário, também adicionado link de cadastro na navbar.

---
+++ Controllers/AutenticacaoController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace AutenticacaoMVC.Controllers
+{
+    public class AutenticacaoController : Controller
+    {
+        
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+    }
+}
