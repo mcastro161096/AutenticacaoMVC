ca844e1a2510e85ba2ded1028d5f7241a100cc36
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-06-16 01:18:59 -0300

    Criando LoginViewModel para utilizar na tela de login.

---
+++ Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AutenticacaoMVC.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string UrlRetorno { get; set; }
+
+        [Required(ErrorMessage = "Informe seu login")]
+        [MaxLength(100, ErrorMessage = "O login deve ter até 100 caracteres")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Informe sua senha")]
+        [MinLength(6, ErrorMessage = "A senha deve ter no mínimo 6 caracteres")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+    }
+}
