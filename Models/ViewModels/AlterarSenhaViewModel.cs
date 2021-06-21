4569e1577b60f320d900ae1e09d9e762fcd7e1fb
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-06-20 21:21:52 -0300

    Criado o viewmodel para alteração de senha.

---
+++ Models/ViewModels/AlterarSenhaViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AutenticacaoMVC.Models.ViewModels
+{
+    public class AlterarSenhaViewModel
+    {
+        [Required(ErrorMessage = "Informe sua senha atual")]
+        [MinLength(6, ErrorMessage = "A senha deve ter no mínimo 6 caracteres")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha atual")]
+        public string SenhaAtual { get; set; }
+
+        [Required(ErrorMessage = "Informe a nova senha")]
+        [MinLength(6, ErrorMessage = "A senha deve ter no mínimo 6 caracteres")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Nova senha")]
+        public string NovaSenha { get; set; }
+
+        [Required(ErrorMessage = "Confirme a nova senha")]
+        [MinLength(6, ErrorMessage = "A senha deve ter no mínimo 6 caracteres")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmação da nova senha")]
+        [Compare(nameof(NovaSenha), ErrorMessage = "A nova senha e a confirmação não estão iguais")]
+        public string ConfirmacaoNovaSenha { get; set; }
+    }
+}
